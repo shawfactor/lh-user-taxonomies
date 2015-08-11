f8d83af267e639d068613cd4638958a0d2b44f65
shawfactor <pete@shawfactor.com>
2015-08-11 17:05:03 +1000

    php fix and tree view
    Props Lindesvard

--- lh-user-taxonomies.php
+++ lh-user-taxonomies.php
@@ -5,37 +5,28 @@ Plugin URI: http://lhero.org/plugins/lh-user-taxonomies/
 Author: Peter Shaw
 Author URI: http://shawfactor.com/
 Description: Simplify the process of adding support for custom taxonomies for Users. Just use `register_taxonomy` and everything else is taken care of. With added functions by Peter Shaw.
-Version:	1.3
-
+Version:	1.4
 == Changelog ==
-
 = 1.0 =
 *Initial Release
-
 = 1.1 =
 *Added icon
-
 = 1.2 =
-*Added various pathches, props nikolaynesov
-
+*Added various patches, props nikolaynesov
 = 1.3 =
 *Better readme.txt
-
+= 1.3.1 =
+*Minor fix, thanks lindesvard
+= 1.4 =
+*Minor fix, and tree logic for hierarchical taxonomies - thanks again Carl-Gerhard Lindesvard
 License:
 Released under the GPL license
 http://www.gnu.org/copyleft/gpl.html
-
 Copyright 2014  Peter Shaw  (email : pete@localhero.biz)
-
-
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published bythe Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
-
 class LH_User_Taxonomies_plugin {
 	private static $taxonomies	= array();
 	
@@ -165,17 +156,51 @@ class LH_User_Taxonomies_plugin {
 	 */
 	public function set_user_column_values($display, $column, $term_id) {
 		if('users' === $column) {
-			$term	= get_term($term_id, $_GET['taxonomy']);
+			$term	= get_term($term_id, $_REQUEST['taxonomy']);
 			echo $term->count;
 		}
 	}
-	
+	private function buildTree( array &$elements, $parentId = 0 ) {
+	    $branch = array();
+	    foreach ($elements as $element) {
+	        if ($element->parent == $parentId) {
+	            $children = $this->buildTree($elements, $element->term_id);
+	            if ($children) {
+	                $element->children = $children;
+	            }
+	            $branch[$element->term_id] = $element;
+	            unset($elements[$element->term_id]);
+	        }
+	    }
+	    return $branch;
+	}
+	private function renderTree( $elements, $stack, $user, $key, $input = 'checkbox' ) {
+		foreach ( $elements as $element ) {
+			?>
+			<div>
+				<input type="<?php echo $input ?>" name="<?php echo $key?>[]" id="<?php echo "{$key}-{$element->slug}"?>" value="<?php echo $element->slug?>" <?php 
+				if ($user->ID){
+					if (in_array($element->slug, $stack)) {
+						echo "checked=\"checked\"";
+					}
+				}
+				?> />
+				<label for="<?php echo "{$key}-{$element->slug}"?>"><?php echo $element->name ?></label>
+				<?php if( isset( $element->children ) ) {
+					?><div style="padding-left: 24px;"><?php
+						$this->renderTree( $element->children, $stack, $user, $key, $input );
+					?></div><?php
+				}
+			?></div><?php
+	    }
+	}
 	/**
 	 * Add the taxonomies to the user view/edit screen
 	 * 
 	 * @param Object $user	- The user of the view/edit screen
 	 */
 	public function user_profile($user) {
+		
 		// Using output buffering as we need to make sure we have something before outputting the header
 		// But we can't rely on the number of taxonomies, as capabilities may vary
 		ob_start();
@@ -183,106 +208,34 @@ class LH_User_Taxonomies_plugin {
 		foreach(self::$taxonomies as $key=>$taxonomy):
 			// Check the current user can assign terms for this taxonomy
 			//if(!current_user_can($taxonomy->cap->assign_terms)) continue;
-			
 			// Get all the terms in this taxonomy
-			$terms	= get_terms($key, array('hide_empty'=>false));
-
-//print_r($terms);
-
-$bars = wp_get_object_terms( $user->ID, $key);
-
-$stack = array();
-
-foreach($bars as $bar){
-
-array_push($stack, $bar->slug);
-
-}
-
-
-
-if (!$taxonomy->single_value){
-
-
-
-
-?>
-
-			<table class="form-table">
-				<tr>
-					<th><label for=""><?php _e("Select {$taxonomy->labels->singular_name}")?></label></th>
-					<td>
-						<?php if(!empty($terms)):?>
-							<?php foreach($terms as $term):?>
-
-
-
-
-
-
-								<input type="checkbox" name="<?php echo $key?>[]" id="<?php echo "{$key}-{$term->slug}"?>" value="<?php echo $term->slug?>" <?php 
-
-if ($user->ID){
-if (in_array($term->slug, $stack)) {
-
-echo "checked=\"checked\"";
-
-}
-}
-
-?> />
-<label for="<?php echo "{$key}-{$term->slug}"?>"><?php echo $term->name?></label> , 
-							<?php endforeach; // Terms?>
-						<?php else:?>
-							<?php _e("There are no {$taxonomy->labels->name} available.")?>
-						<?php endif?>
-					</td>
-				</tr>
-			</table>
-
-<?php
-
-} else {
-
+			$terms		= get_terms($key, array('hide_empty'=>false));
+			$stack 		= wp_list_pluck( wp_get_object_terms( $user->ID, $key ), 'slug' );
+			$input_type = ( $taxonomy->single_value ) ? 'radio' : 'checkbox' ;
 			?>
-			<table class="form-table">
-				<tr>
-					<th><label for=""><?php _e("Select {$taxonomy->labels->singular_name}")?></label></th>
-					<td>
-						<?php if(!empty($terms)):?>
-							<?php foreach($terms as $term):?>
-								<input type="radio" name="<?php echo $key?>" id="<?php echo "{$key}-{$term->slug}"?>" value="<?php echo $term->slug?>" <?php 
-if ($user->ID){
-if (in_array($term->slug, $stack)) {
 
-echo "checked=\"checked\"";
+				<table class="form-table">
+					<tr>
+						<th><label for=""><?php _e("Select {$taxonomy->labels->singular_name}")?></label></th>
+						<td>
+							<?php if(!empty($terms)): ?>
 
-}
-}
+								<?php $this->renderTree( $this->buildTree( $terms ), $stack, $user, $key, $input_type ); ?>
 
+							<?php else:?>
+								<?php _e("There are no {$taxonomy->labels->name} available.")?>
+							<?php endif?>
+						</td>
+					</tr>
+				</table>
 
-
-
-?> />
-								<label for="<?php echo "{$key}-{$term->slug}"?>"><?php echo $term->name?></label>
-							<?php endforeach; // Terms?>
-						<?php else:?>
-							<?php _e("There are no {$taxonomy->labels->name} available.")?>
-						<?php endif?>
-					</td>
-				</tr>
-			</table>
-			<?php }
-		endforeach; // Taxonomies
-		
+		<?php endforeach; // Taxonomies
 		// Output the above if we have anything, with a heading
 		$output	= ob_get_clean();
 		if(!empty($output)) {
 			echo '<h3>', __('Taxonomies'), '</h3>';
 			echo $output;
 		}
-
-
 	}
 	
 	/**
@@ -291,27 +244,19 @@ echo "checked=\"checked\"";
 	 * @param Integer $user_id	- The ID of the user to update
 	 */
 public function save_profile($user_id) {
-
 		foreach(self::$taxonomies as $key=>$taxonomy) {
 			// Check the current user can edit this user and assign terms for this taxonomy
 			if(!current_user_can('edit_user', $user_id) && current_user_can($taxonomy->cap->assign_terms)) return false;
-
 				if (isset($_POST[$key])) {
 					if (is_array($_POST[$key])){
-
 						$term = $_POST[$key];
-
 						wp_set_object_terms($user_id, $term, $key, false);
-
 					} else {
-
 						$term	= esc_attr($_POST[$key]);
 						wp_set_object_terms($user_id, array($term), $key, false);
-
 					}
 				}
 				// Save the data
-
 			clean_object_term_cache($user_id, $key);
 		}
 	}
@@ -326,38 +271,26 @@ public function save_profile($user_id) {
 		
 		return $username;
 	}
-
 	/**
 	 * Add columns for columns with
 	 * show_admin_column
 	 */
 	public function lh_user_taxonomies_add_user_id_column($columns) {
-
 $args=array(
   'object_type' => array('user'),
 'show_admin_column' => true
 );
-
-
 $taxonomies = get_taxonomies( $args, "objects");
-
 foreach ($taxonomies as $taxonomy) {
-
-
 $columns[$taxonomy->name] = $taxonomy->labels->name;
-
 }
-
-
     return $columns;
 }
-
 	/**
 	 * Just a private function to
 	 * populate column content
 	 */
 	private function lh_user_taxonomies_get_user_taxonomies($user, $taxonomy, $page = null) {
-
 $terms = wp_get_object_terms( $user, $taxonomy);
 		if(empty($terms)) { return false; }
 		$in = array();
@@ -365,95 +298,48 @@ $terms = wp_get_object_terms( $user, $taxonomy);
 			$href = empty($page) ? add_query_arg(array($taxonomy => $term->slug), admin_url('users.php')) : add_query_arg(array('user-group' => $term->slug), $page);
 			$in[] = sprintf('%s%s%s', '<a href="'.$href.'" title="'.esc_attr($term->description).'">', $term->name, '</a>');
 		}
-
 	  	return implode('', $in);
 	}
-
-
 	/**
 	 * Add the column content
 	 * 
 	 */
-
 	public function lh_user_taxonomies_add_taxonomy_column_content($value, $column_name, $user_id) {
-
 if (taxonomy_exists($column_name)) {
-
 return $this->lh_user_taxonomies_get_user_taxonomies($user_id,$column_name);
-
 } else {
     return $value;
 }
-
-
 }
 	/**
 	 * Alters the User query
 	 * to return a different list based on query vars on users.php
 	 */
-
 	public function user_query($Query = '') {
 		global $pagenow,$wpdb;
-
 if ( $pagenow == 'users.php' ){
-
-
 $args=array(
   'object_type' => array('user'),
 'show_admin_column' => true
 );
-
-
 $taxonomies = get_taxonomies( $args, "objects");
-
-
-
-
 foreach ($taxonomies as $taxonomy) {
-
 if(!empty($_GET[$taxonomy->name])) {
-
-
-
 $term = get_term_by('slug', esc_attr($_GET[$taxonomy->name]), $taxonomy->name);
-
-
-
 $new_ids = get_objects_in_term($term->term_id, $taxonomy->name);
-
-
-
 if (!isset($ids) || empty($ids)){  
-
 $ids = $new_ids;  
-
 } else {   
-
 $ids = array_intersect($ids, $new_ids);
-
 }
-
-
 }
-
 }
-
 if ( isset( $ids ) ){  
-
 $ids = implode(',', wp_parse_id_list( $ids ) );
-
 $Query->query_where .= " AND $wpdb->users.ID IN ($ids)";
-
 }
-
 }		
-
 	
 }
-
-
-
-
 }
-
 new LH_User_Taxonomies_plugin;
